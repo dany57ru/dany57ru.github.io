f2e50a3bdebc40616257750433e65815d1ac5437
dany57ru <127332379+dany57ru@users.noreply.github.com>
2023-05-19 10:52:58 +0300

    a

--- Java_lab2/lab2.js
+++ Java_lab2/lab2.js
@@ -9,7 +9,13 @@ function sumTo(n)
 }
 function factorial(n)
 {
-    return BigInt(n ? (n * factorial(n-1)) : 1) ;
+    if (n<=0)
+    {
+        return 1n;
+    }
+    else{
+        return ( BigInt (n) *  BigInt(factorial(n-1)));
+    }
 }
 function fib(n){
     a = BigInt(1);
@@ -28,4 +34,22 @@ function fib(n){
         }
         return b;
     }
+}
+function compare(x)
+{
+return function(y)
+{
+    if (y>x) return true;
+    else if (y<x) return false;
+    else if (y==x) return null;
+}
+}
+function sum()
+{
+    var result=0;
+    for (var i=0; i<arguments.length; i++)
+    {
+        result+=arguments[i]
+    }
+    return result;
 }
