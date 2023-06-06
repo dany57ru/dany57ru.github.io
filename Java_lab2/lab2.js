4ac0063f0198602f48120ff995cf846b87758e1a
dany57ru <127332379+dany57ru@users.noreply.github.com>
2023-06-06 12:20:30 +0300

    q

--- Java_lab2/lab2.js
+++ Java_lab2/lab2.js
@@ -1,56 +1,91 @@
-function pow(x, n)
-{
-    return x**n;
+function pow(x, n) {
+    return x ** n;
 }
-function sumTo(n)
 
-{
-    return (n*(n+1))/2
+/**
+ * Возвращает a, возведённое в b-ную степень.
+ *
+ * @param {number} a Возводимое в степень число.
+ * @param {number} b Степень, должна быть натуральным числом.
+ * @return {number} a ** b, возведённое в n-ную степень.
+ */
+
+function sumTo(n) {
+    return (n * (n + 1)) / 2
 
 }
-function factorial(n)
-{
-    if (n<=0)
-    {
+
+/**
+ * Возвращает сумму чисел от 1 до n включительно.
+ * @param {number} n Число, до которого вычисляется сумма, должно быть натуральным числом.
+ * @return {number} sum, сумму чисел от 1 до n включительно.
+ */
+
+function factorial(n) {
+    if (n <= 0) {
         return 1n;
     }
-    else{
-        return ( BigInt (n) *  BigInt(factorial(n-1)));
+    else {
+        return (BigInt(n) * BigInt(factorial(n - 1)));
     }
 }
-function fib(n){
+
+/**
+ * Возвращает факториал числа n!.
+ * @param {number} n Число, факториал которого вычисляется, должно быть натуральным числом.
+ * @return {number} res, факториал числа n!.
+ */
+
+function fib(n) {
     a = BigInt(1);
-    b=BigInt(1);
-    if (n== BigInt(0))
-    {
+    b = BigInt(1);
+    if (n == BigInt(0)) {
         return BigInt(0);
     }
-    else
-    {
-        for (i = BigInt(3); i<=n; i++)
-        {
-            c = a+b;
-            a=b;
-            b=c;
+    else {
+        for (i = BigInt(3); i <= n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
         }
         return b;
     }
 }
-function compare(x)
-{
-return function(y)
-{
-    if (y>x) return true;
-    else if (y<x) return false;
-    else if (y==x) return null;
+
+/**
+ * Возвращает n-е число Фибоначчи.
+ * @param {number} n Порядковый номер числа Фибоначчи, должен быть натуральным числом.
+ * @return {number} b, n-е число Фибоначчи.
+ */
+
+
+function compare(x) {
+    return function (y) {
+        if (y > x) return true;
+        else if (y < x) return false;
+        else if (y == x) return null;
+    }
 }
-}
-function sum()
-{
-    var result=0;
-    for (var i=0; i<arguments.length; i++)
-    {
-        result+=arguments[i]
+
+/**
+ * Возвращает результат сравнения значений y и x.
+ * @param {number} x Первый аргумент, должен быть целочисленным.
+ * @param {number} y Второй аргумент, должен быть целочисленным.
+ * @return {function} Анонимная функция, которая вернет true, false или null
+ }} Результат сравнения.
+ */
+
+
+function sum() {
+    var result = 0;
+    for (var i = 0; i < arguments.length; i++) {
+        result += arguments[i]
     }
     return result;
 }
+
+/**
+ * Возвращает сумму всех своих аргументов.
+ * @param {number} Значения аргументов.
+ * @return {number} result, сумма всех своих аргументов.
+ */
